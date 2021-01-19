e749c99e6cdf3cf05a4292082443fc199446900f
Hedgehog-Man <77652329+Hedgehog-Man@users.noreply.github.com>
2021-01-18 19:41:12 -0700

    Add files via upload

---
+++ stuffystuff.js
@@ -0,0 +1,12 @@
+function buttons{
+   window.window.location = "" 
+}
+function game{
+  window.window.location = ""   
+}
+function contacts{
+ window.window.location = ""    
+}
+function hobs{
+ window.window.location = "https://f8hm9b4qjrxm10cuz0mqpa-on.drv.tw/game%26project/stuff.html"    
+}
